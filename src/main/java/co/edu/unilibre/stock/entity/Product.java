78d692303a5a3150c891e5a385c4176ec5b0d069
casallas123 <andresf-casallasa@unilibre.edu.co>
2019-10-12 19:34:21 -0500

    Creacion BD sin resultado

--- src/main/java/co/edu/unilibre/stock/entity/Product.java
+++ src/main/java/co/edu/unilibre/stock/entity/Product.java
@@ -1,7 +1,8 @@
-package co.edu.unilibre.inventario.entity;
+package co.edu.unilibre.stock.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,18 +13,17 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name = "inventarys")
-public class Inventario implements Serializable{
+@Table(name = "products")
+public class Product implements Serializable{
 
 	private static final long serialVersionUID = -1462302884002834226L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nombre;
-	private Double precio;
-	private String unidad;//(KG,L,G)
-	private String provedor;
+	
+	private String name;
+	private Double price;
 	
 	@Column(name = "creation_date")
 	@Temporal(TemporalType.DATE)
@@ -37,36 +37,20 @@ public class Inventario implements Serializable{
 		this.id = id;
 	}
 
-	public String getNombre() {
-		return nombre;
+	public String getName() {
+		return name;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public Double getPrecio() {
-		return precio;
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setPrecio(Double precio) {
-		this.precio = precio;
-	}
-
-	public String getUnidad() {
-		return unidad;
-	}
-
-	public void setUnidad(String unidad) {
-		this.unidad = unidad;
-	}
-
-	public String getProvedor() {
-		return provedor;
-	}
-
-	public void setProvedor(String provedor) {
-		this.provedor = provedor;
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 
 	public Date getCreateDate() {
@@ -76,8 +60,6 @@ public class Inventario implements Serializable{
 	public void setCreateDate(Date createDate) {
 		this.createDate = createDate;
 	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+	
+	
 }
