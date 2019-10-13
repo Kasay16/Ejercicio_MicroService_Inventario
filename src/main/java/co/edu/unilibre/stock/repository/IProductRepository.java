a60c4f14b8519894b4b808806fbc55e7a7a0dc64
casallas123 <andresf-casallasa@unilibre.edu.co>
2019-10-12 20:05:19 -0500

    Actualizacion de paquetes y clase controller organizada

--- src/main/java/co/edu/unilibre/stock/repository/IProductRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unilibre.stock.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.unilibre.stock.entity.Product;
-
-public interface IProductRepository extends JpaRepository<Product, Long>{
-
-}
